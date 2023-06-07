d1b6e56d0eaa2be18f075b23ef90c01cc3f2b472
Abhishek Singh Sengar <theabhissengar@gmail.com>
2023-06-07 18:11:11 +0530

    Update Nav.jsx

--- src/Components/nav/Nav.jsx
+++ src/Components/nav/Nav.jsx
@@ -9,10 +9,16 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
 
   const [activeNav, setActiveNav] = useState('#home')
+  const linkStyle = {
+    margin: "0",
+    padding: "0",
+    textDecoration: "none",
+  };
+
 
   return (
     <nav>
-    <Link to="/" style={{textDecoration:"none"}}>
+    <Link to="/" style={linkStyle} >
       <a href="#home" onClick={() =>setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiOutlineHome/></a></Link>
       <a href="#about" onClick={() =>setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} ><AiOutlineUser/></a>
       <a href="#view" onClick={() =>setActiveNav('#view')} className={activeNav === '#view' ? 'active' : ''} ><VscPreview/></a>
